db131cc7189ff737d5a0f67eb40128a7e763da1b
anitkhan233 <anit.cse@gmail.com>
2024-11-12 10:36:33 +1100

    Added Animation

--- Ball Move With Arrow Keys/js/gamelet.js
+++ Ball Move With Arrow Keys/js/gamelet.js
@@ -2,6 +2,10 @@ let ball; //it stores the ball object
 let ballContainerDiv; //it stores the div object which is the container of the ball. The Ball moves within this container
 let ball_position_top; //presents the value of top position of the ball
 let ball_position_left; //presents the value of left position of the ball
+let id = null; //for animation
+let changed_ball_position_top; //presents resultant top position value of the ball after moving 200px up or down
+let changed_ball_position_left; //presents resultant left position value of the ball after moving 200px left or right
+        
 
 /*
     function initializeVariables() initializes all the above mentioned variables
@@ -11,6 +15,8 @@ function initializeVariables()
     console.log("The Event 'Body Element Load' has been fired");
     ball_position_top = 0;
     ball_position_left = 0;
+    changed_ball_position_top = 0;
+    changed_ball_position_left = 0;
     ball = document.querySelector("#ball");
     ballContainerDiv = document.querySelector("#ball_container_div");
 }
@@ -22,37 +28,105 @@ function handleKeyPress(event)
 {
     if(event.key === "ArrowUp")
     {
-        ball_position_top = ball_position_top - 10;
-        if(ball_position_top < 0)
+        changed_ball_position_top = ball_position_top - 200;
+        if(changed_ball_position_top < 0)
         {
-            ball_position_top = 0;
+            changed_ball_position_top = 0;
         }
-        displayBall();
+        id = setInterval(moveBallUp, 5);
     }
     if(event.key === "ArrowDown")
     {
-        ball_position_top = ball_position_top + 10;
-        if(ball_position_top > (ballContainerDiv.clientHeight - ball.clientHeight) )
+        changed_ball_position_top = ball_position_top + 200;
+        if(changed_ball_position_top > (ballContainerDiv.clientHeight - ball.clientHeight))
+        {
+            changed_ball_position_top = (ballContainerDiv.clientHeight - ball.clientHeight);
+        }
+        id = setInterval(moveBallDown, 5);
+    }
+    if(event.key === "ArrowRight")
+    {
+        changed_ball_position_left = ball_position_left + 200;
+        if(changed_ball_position_left > (ballContainerDiv.clientWidth - ball.clientWidth))
+        {
+            changed_ball_position_left = (ballContainerDiv.clientWidth - ball.clientWidth);
+        }
+        id = setInterval(moveBallRight, 5);
+    }
+    if(event.key === "ArrowLeft")
+    {
+        changed_ball_position_left = ball_position_left - 200;
+        if(changed_ball_position_left < 0)
+        {
+            changed_ball_position_left = 0;
+        }
+        id = setInterval(moveBallLeft, 5);
+    }
+}
+
+function moveBallUp()
+{
+    if((ball_position_top == changed_ball_position_top))
+    {
+        clearInterval(id);
+    }
+    else
+    {
+        ball_position_top--;
+        if(ball_position_top < 0)
+        {
+            ball_position_top = 0;
+        }      
+        displayBall();
+    }
+}
+
+function moveBallDown()
+{
+    if((ball_position_top == changed_ball_position_top))
+    {
+        clearInterval(id);
+    }
+    else
+    {
+        ball_position_top++;
+        if(ball_position_top > (ballContainerDiv.clientHeight - ball.clientHeight))
         {
             ball_position_top = ballContainerDiv.clientHeight - ball.clientHeight;
         }
         displayBall();
     }
-    if(event.key === "ArrowRight")
+}
+
+function moveBallLeft()
+{
+    if((ball_position_left == changed_ball_position_left))
     {
-        ball_position_left = ball_position_left + 10;
-        if(ball_position_left > (ballContainerDiv.clientWidth - ball.clientWidth))
-        {
-            ball_position_left = ballContainerDiv.clientWidth - ball.clientWidth;
-        }
-        displayBall();
+        clearInterval(id);
     }
-    if(event.key === "ArrowLeft")
+    else
     {
-        ball_position_left = ball_position_left - 10;
+        ball_position_left--;
         if(ball_position_left < 0)
         {
-            ball_position_left = 0;
+            ball_position_top = 0;
+        }      
+        displayBall();
+    }
+}
+
+function moveBallRight()
+{
+    if((ball_position_left == changed_ball_position_left))
+    {
+        clearInterval(id);
+    }
+    else
+    {
+        ball_position_left++;
+        if(ball_position_left > (ballContainerDiv.clientWidth - ball.clientWidth))
+        {
+            ball_position_left = (ballContainerDiv.clientWidth - ball.clientWidth);
         }
         displayBall();
     }
